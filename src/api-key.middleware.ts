4605371032f03b8e8129e17b13355a0d9f6f12d1
Sơn-Nguyên <nguyenpts2003@gmail.com>
2025-05-22 09:54:16 +0700

    [cd-giavang-api]

--- src/api-key.middleware.ts
+++ src/api-key.middleware.ts
@@ -4,8 +4,18 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class ApiKeyMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
+    // Loại trừ các route public không cần API Key
+    const publicPaths = [
+      '/auth/google',
+      '/auth/google/callback',
+      '/auth/create-test-account',
+    ];
+    if (publicPaths.includes(req.path)) {
+      return next();
+    }
+
     const apiKey = req.headers['x-api-key'] || req.query.api_key;
-    const validApiKey = process.env.API_KEY ;
+    const validApiKey = process.env.API_KEY;
 
     if (apiKey !== validApiKey) {
       throw new UnauthorizedException('Invalid API Key');
